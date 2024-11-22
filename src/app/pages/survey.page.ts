3934594cd2074fa115ade4c45962a5ba949b526d
VincenzoManto <vincmanto@gmail.com>
2024-11-22 23:17:45 +0100

    refactor SurveyPage to use new survey component and update data handling logic

--- src/app/pages/survey.page.ts
+++ src/app/pages/survey.page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 import { json } from "../data/survey";
 import { HttpClient } from "@angular/common/http";
+import { Model, SurveyModel } from "survey-core";
 
 @Component({
   selector: "survey-page",
@@ -12,17 +13,40 @@ export class SurveyPage {
   data;
   machineCode = localStorage.getItem("machineCode");
   group;
+  model;
   step = 1;
 
   constructor(private http: HttpClient) {
     this.json = json;
-    this.http.get("https://donationsexperiment-default-rtdb.europe-west1.firebasedatabase.app/.json").subscribe((data: any) => {
-      this.data = [];
-      if (data[this.machineCode]) {
-        // completed survey
-      }
-      Object.values(data).forEach(e => this.data.push(...Object.values(e)));
+    this.model = new Model(this.json); // Object.values(data[this.machineCode])[0];
+    this.model.cookieName = this.machineCode;
+    this.model.showPrevButton = false;
+    this.model.onValueChanged.add((sender, options) => {
+      localStorage.setItem("progress", JSON.stringify(sender.getData()));
+      localStorage.setItem("currentPage", JSON.stringify(sender.currentPageNo));
     });
+
+    this.model.onCurrentPageChanged.add((sender, options) => {
+      localStorage.setItem("currentPage", JSON.stringify(sender.currentPageNo));
+    });
+    this.http
+      .get(
+        "https://donationsexperiment-default-rtdb.europe-west1.firebasedatabase.app/.json"
+      )
+      .subscribe((data: any) => {
+        this.data = [];
+        if (data[this.machineCode]) {
+          this.step = 2;
+          this.model.doComplete();
+        } else if (localStorage.getItem("progress")) {
+          this.model.mergeData(
+            JSON.parse(localStorage.getItem("progress")) || {}
+          );
+          this.model.currentPageNo =
+            JSON.parse(localStorage.getItem("currentPage")) || 0;
+        }
+        Object.values(data).forEach((e) => this.data.push(...Object.values(e)));
+      });
     if (!this.machineCode) {
       this.machineCode = this.generateMachineCode();
       localStorage.setItem("machineCode", this.machineCode);
@@ -37,8 +61,11 @@ export class SurveyPage {
 
   sendData(result) {
     console.log(result);
-    this.http.post(
-      `https://donationsexperiment-default-rtdb.europe-west1.firebasedatabase.app/${this.machineCode}.json`, result).subscribe(() => {
-    });
+    this.http
+      .post(
+        `https://donationsexperiment-default-rtdb.europe-west1.firebasedatabase.app/${this.machineCode}.json`,
+        result
+      )
+      .subscribe(() => {});
   }
 }
