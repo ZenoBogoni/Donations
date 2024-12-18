47736517db3e7beef1e9a6166cf7aeea9c1743db
VincenzoManto <vincmanto@gmail.com>
2024-12-18 12:54:55 +0100

    close #4 - migliorie UI leaderboard

--- src/app/pages/game.component.ts
+++ src/app/pages/game.component.ts
@@ -321,9 +321,7 @@ export class GameComponent implements OnInit, AfterViewInit {
           this.machineCode + "/donation" + this.currentMatch
         ),
         {
-          name: this.donation.name,
-          donation: this.donation.amount,
-          lives: this.donation.lives,
+          ...this.donation
         }
       ),
     ];
